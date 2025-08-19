9d13d7045d89be5159dbdb4a0669d703b8b1c8aa
hiroto <hiroto8430@gmail.com>
2025-08-20 08:07:10 +0900

    first commit.

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
+from langchain_openai import ChatOpenAI
+from langchain.schema import SystemMessage, HumanMessage
 
 st.title("旅行チャットボット")
-
 st.write("##### 希望の専門家を選択してください")
 st.write("A:観光スポットに詳しい専門家")
 st.write("B:グルメに詳しい専門家")
@@ -16,6 +17,12 @@ input_message = st.text_input(label="質問を入力してください")
 
 #LangChainを使ってLLMにプロンプトを渡す
 if st.button("実行"):
+    llm = ChatOpenAI(model_name="gpt-4o-mini", temperature=0)
+    messages = [
+    SystemMessage(content=selected_item),
+    HumanMessage(content=input_message),]
+    result = llm(messages)
+    return result.content
 
 
 
